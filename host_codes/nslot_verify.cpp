8afc1ff657b66e75b04a2b7240456ac19c993c8c
Ubuntu <ubuntu@ip-172-31-24-126.us-east-2.compute.internal>
2021-11-15 09:37:01 +0000

    nslot_verify fixed

--- host_codes/nslot_verify.cpp
+++ host_codes/nslot_verify.cpp
@@ -9,46 +9,58 @@
 #include "gpudiag_runtime.h"
 #include <vector>
 
-#define test_until_multiple 3
+#define num_mp 1
+#define test_until_multiple 7
 
 uint64_t test_once(uint64_t G, int B) {
-    uint64_t htime, *dtime;
-    GDMalloc(&dtime, sizeof(uint64_t));
-    htime = 0;
-    GDMemcpyHToD(dtime, &htime, sizeof(uint64_t));
+    uint64_t *htime, *dtime;
+    size_t arrsize = 2 * G * sizeof(uint64_t);
+    htime = (uint64_t*)malloc(arrsize);
+    GDMalloc(&dtime, arrsize);
+    for (int i=0; i<G; i++) htime[i] = 0;
+    GDMemcpyHToD(dtime, htime, arrsize);
     GDLaunchKernel(measure_width_verify, dim3(G), dim3(B), 0, 0, dtime);
     GDSynchronize();
-    GDMemcpyDToH(&htime, dtime, sizeof(uint64_t));
+    GDMemcpyDToH(htime, dtime, arrsize);
     GDFree(dtime);
-    return htime;
+    uint64_t minsclk=(uint64_t)-1, maxeclk=0;
+    for (int i=0; i<G; i++) {
+        uint64_t sclk = htime[2*i], eclk = htime[2*i+1];
+        minsclk = minsclk < sclk ? minsclk : sclk;
+        maxeclk = maxeclk > eclk ? maxeclk : eclk;
+    }
+    free(htime);
+    return maxeclk - minsclk;
 }
 
 int main(int argc, char **argv) {
     GDInit();
     
     const int iter_num = limit_threads_per_block(0) / warp_size(0);
-    std::vector<uint64_t> results[iter_num];
+    std::vector<float> results[iter_num];
 
     uint64_t value = 0;
     for (int b=1; b<=iter_num; b++) {
         int testB = b * warp_size(0);
         uint64_t testG = 1;
         uint64_t ref_val = test_once(testG, testB);
-        results[b-1].push_back(ref_val);
-        testG++;
+        results[b-1].push_back(1.0f);
+        testG += num_mp;
         while(1) {
             value = test_once(testG, testB);
-            results[b-1].push_back(value);
+            results[b-1].push_back((float)value / (float)ref_val);
             if (value > ref_val * test_until_multiple) break;
-            testG++;
+            testG += num_mp;
         }
     }
 
     // print
+    FILE *out = fopen("nslot_verify_result.txt", "w");
     for (int b=1; b<=iter_num; b++) {
         for (int i=0; i<results[b-1].size(); i++) {
-            printf("%lu,", results[b-1][i]);
+            fprintf(out, "%f,", results[b-1][i]);
         }
-        printf("\n");
+        fprintf(out, "\n");
     }
+    fclose(out);
 }
--- tests/nslot_verify.py
+++ tests/nslot_verify.py
@@ -1,6 +1,6 @@
 from .test_template import test_template
 from .define import Test
-import functional_units
+from . import functional_units
 import sys, os
 
 ### ========== Options for nslot_verify test! ==========
@@ -8,7 +8,43 @@ import sys, os
 nvidia_inst_to_test = "shmem"
 amd_inst_to_test = "shmem"
 ### How many for repetitions?
-for_repetitions = 100
+for_repetitions = 1000
+
+### ========== End of Options ==========
+
+def measure_width_multiple_G_code(repeat, repeat_for, inicode, repcode, fincode):
+    code = f"""\
+__gdkernel void measure_width_verify(__gdbufarg uint64_t *result) {{
+    uint64_t sclk, eclk;
+    {inicode}
+    int repeats = 1;
+#pragma unroll 1
+    for (int i=0; i<2; i++) {{
+        if (i==1) {{ // icache warmup done
+            repeats = {repeat_for};
+            GDsyncthreads();
+            sclk = GDclock();
+        }}
+#pragma unroll 1
+        for (int j=0; j<repeats; j++) {{
+            asm volatile(
+"""
+    for i in range(repeat):
+        code += repcode(i)
+    code += fincode
+    code += f"""\
+            );
+        }}
+        GDsyncthreads();
+        eclk = GDclock();
+    }}
+    if (GDThreadIdx == 0) {{
+        result[2*GDBlockIdx] = sclk;
+        result[2*GDBlockIdx+1] = eclk;
+    }}
+}}
+"""
+    return code
 
 class nslot_verify (test_template):
     test_enum = Test.nslot_verify
@@ -24,9 +60,9 @@ class nslot_verify (test_template):
             if fut.name == target:
                 target_fut = fut
                 break
-        code += functional_units.measure_width_code(
+        code += measure_width_multiple_G_code(
             target_fut.width_rep, for_repetitions,
-            "verify", target_fut.inicode, target_fut.repcode2,
+            target_fut.inicode, target_fut.repcode2,
             target_fut.fincode2(target_fut.width_rep))
         code += f"""\
 #define TEST_INST_NAME "{target_fut.name}"
